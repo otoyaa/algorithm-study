5a45736382c9e6494560603ff781fc6a8071018b
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-11-02 09:00:13 -0300

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,14 @@
+                /* 🔰 Natus*/
+#include <bits/stdc++.h>          
+#define endl '\n';
+typedef long long ll;
+using namespace std;
+
+void solve(){
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);   
+    solve();
+}
