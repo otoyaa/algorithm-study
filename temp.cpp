7d4416160cfcc307aa9361cc89306d199b6dc271
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-12-27 16:23:11 -0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -5,7 +5,12 @@ using namespace std;
 #define sbt(x) (x).begin(),(x).end()
 #define gyaku(x) reverse((x).begin(),(x).end())
 typedef long long ll;
+
 const ll mod=1000000007;
+const ll INF= 1LL << 60;
+
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
 
 struct 光線{
     光線(){
@@ -17,3 +22,4 @@ struct 光線{
 
 int main(){
 }
+
