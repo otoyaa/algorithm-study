b703f1ed6e8253ba8027bfb52a04079e0ab419b6
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-12-29 17:37:47 -0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,16 +1,27 @@
-/*震えているんじゃなくて 奮い立っているんだと*/
 #include <bits/stdc++.h>    
 using namespace std;
 
 #define sbt(x) (x).begin(),(x).end()
 #define gyaku(x) reverse((x).begin(),(x).end())
 typedef long long ll;
+typedef unsigned long long ull;
 
 const ll mod=1000000007;
 const ll INF= 1LL << 60;
 
 template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
 template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+
+ull mp(ull a,ull b,ull mod){
+    ull p=a,ans=1;
+    for(int i=0;i<60;++i){
+        if((b&(1ll<<i))!=0){
+            ans*=p;ans%=mod;
+        }
+        p*=p;p%=mod;
+    }
+    return ans;
+}
 
 struct 光線{
     光線(){
@@ -22,4 +33,3 @@ struct 光線{
 
 int main(){
 }
-
